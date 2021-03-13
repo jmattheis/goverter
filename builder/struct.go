1d7f8091fb84611d4c1c94f13a5b14f70c9b4d15
Jannis Mattheis <contact@jmattheis.de>
2021-03-13 13:49:02 +0100

    Add struct builder

---
+++ builder/struct.go
@@ -0,0 +1,61 @@
+package builder
+
+import (
+	"fmt"
+	"go/types"
+
+	"github.com/dave/jennifer/jen"
+)
+
+type Struct struct{}
+
+func (*Struct) Matches(source, target *Type) bool {
+	return source.Struct && target.Struct
+}
+
+func (*Struct) Build(gen Generator, ctx *MethodContext, sourceID JenID, source, target *Type) ([]jen.Code, JenID, *Error) {
+	name := ctx.Of(target, "structTarget")
+	stmt := []jen.Code{
+		jen.Var().Id(name).Add(target.TypeAsJen()),
+	}
+
+	sourceStruct := source.StructType
+	targetStruct := target.StructType
+
+	sourceMethods := map[string]*types.Var{}
+	for i := 0; i < sourceStruct.NumFields(); i++ {
+		m := sourceStruct.Field(i)
+		sourceMethods[m.Name()] = m
+	}
+
+	for i := 0; i < targetStruct.NumFields(); i++ {
+		targetField := targetStruct.Field(i)
+		sourceField, ok := sourceMethods[targetField.Name()]
+		if !ok {
+			cause := fmt.Sprintf("Cannot set value for field %s because no it does not exist on the source entry", targetField.Name())
+			return nil, nil, NewError(cause).Lift(&Path{
+				Prefix:     ".",
+				SourceID:   "???",
+				TargetID:   targetField.Name(),
+				TargetType: targetField.Type().String(),
+			})
+		}
+
+		fieldSourceId := sourceID.Clone().Dot(sourceField.Name())
+
+		fieldStmt, fieldID, err := gen.Build(ctx, fieldSourceId, TypeOf(sourceField.Type()), TypeOf(targetField.Type()))
+		if err != nil {
+			return nil, nil, err.Lift(&Path{
+				Prefix:     ".",
+				SourceID:   sourceField.Name(),
+				TargetID:   targetField.Name(),
+				TargetType: sourceField.Type().String(),
+				SourceType: targetField.Type().String(),
+			})
+		}
+		stmt = append(stmt, fieldStmt...)
+		stmt = append(stmt, jen.Id(name).Dot(targetField.Name()).Op("=").Add(fieldID))
+	}
+
+	return stmt, jen.Id(name), nil
+}
