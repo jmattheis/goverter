0a3622552c437f561e0e86b5b161b6526e0adfbe
Jannis Mattheis <contact@jmattheis.de>
2024-02-22 18:39:24 +0100

    fix: simplify cli run

--- cmd/goverter/main.go
+++ cmd/goverter/main.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"os"
 
-	goverter "github.com/jmattheis/goverter"
 	"github.com/jmattheis/goverter/cli"
 )
 
 func main() {
-	cfg, err := cli.Parse(os.Args)
-	if err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	if err = goverter.GenerateConverters(cfg); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	cli.Run(os.Args, cli.RunOpts{})
 }
