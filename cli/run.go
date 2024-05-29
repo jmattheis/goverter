acbb7a40a31a984fd8b042de72dfcfe350e085c6
Jannis Mattheis <contact@jmattheis.de>
2024-05-29 21:39:37 +0200

    fix(cli): add `help` and `version`

--- cli/run.go
+++ cli/run.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"fmt"
 	"os"
+	"runtime/debug"
 
 	"github.com/jmattheis/goverter"
 	"github.com/jmattheis/goverter/enum"
@@ -14,20 +15,33 @@ type RunOpts struct {
 
 // Run runs the goverter cli with the given args and customizations.
 func Run(args []string, opts RunOpts) {
-	cfg, err := Parse(args)
+	cmd, err := Parse(args)
 	if err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	if opts.EnumTransformers != nil {
-		for key, value := range opts.EnumTransformers {
-			cfg.EnumTransformers[key] = value
+	switch cmd := cmd.(type) {
+	case *Help:
+		_, _ = fmt.Fprintln(os.Stdout, cmd.Usage)
+		os.Exit(0)
+	case *Generate:
+		if opts.EnumTransformers != nil {
+			for key, value := range opts.EnumTransformers {
+				cmd.Config.EnumTransformers[key] = value
+			}
 		}
-	}
 
-	if err = goverter.GenerateConverters(cfg); err != nil {
-		_, _ = fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+		if err = goverter.GenerateConverters(cmd.Config); err != nil {
+			_, _ = fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case *Version:
+		b, ok := debug.ReadBuildInfo()
+		if ok {
+			fmt.Println(b)
+		}
+	default:
+		panic("unknown command")
 	}
 }
