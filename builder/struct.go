bc67278ca44b91ceced601566b7c0e6a5312d991
Rangel Reale <rangelspam@gmail.com>
2026-07-14 09:42:03 -0300

    debug comments

--- builder/struct.go
+++ builder/struct.go
@@ -46,8 +46,7 @@ func (s *Struct) Assign(gen Generator, ctx *MethodContext, assignTo *AssignTo, s
 
 		if fieldMapping.Ignore {
 			if ctx.Conf.DebugComments {
-				// stmt = append(stmt, jen.Commentf("ignore: %s", targetField.Name()))
-				stmt = append(stmt, jen.Commentf("ignore: %s", assignTo.Stmt.Clone().Dot(targetField.Name()).GoString()))
+				stmt = append(stmt, jen.Commentf("ignored: %s", assignTo.Stmt.Clone().Dot(targetField.Name()).GoString()))
 			}
 			continue
 		}
